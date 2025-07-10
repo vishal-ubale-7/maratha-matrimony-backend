105bf7a9d5b4be6c14ccd0d7c23a5507b8086fc6
Mr.Vishal Ubale <149001221+vishal-ubale-7@users.noreply.github.com>
2025-07-10 12:20:15 +0530

    git"commit"

--- src/main/java/com/matrimony/Config/SecurityConfig.java
+++ src/main/java/com/matrimony/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.matrimony.Config;
 
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig
 {
--- src/main/java/com/matrimony/Config/SwaggerConfig.java
+++ src/main/java/com/matrimony/Config/SwaggerConfig.java
@@ -4,15 +4,18 @@ package com.matrimony.Config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SwaggerConfig
 {
     @Bean
-    public OpenAPI customOpenAPI()
-    {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Maratha Matrimony API")
@@ -20,7 +23,16 @@ public class SwaggerConfig
                         .description("This is the API documentation for the Maratha Matrimony project")
                         .contact(new Contact()
                                 .name("Vishal Ubale")
-                                .email("vishal@example.com")));
+                                .email("vishal@example.com"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
 
